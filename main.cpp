97433a37cad4c9df2b5d66eeb0bf63356b59f015
DJjanko <jankladusek7@gmail.com>
2024-03-31 19:36:53 +0200

    added kmp_next

--- main.cpp
+++ main.cpp
@@ -32,10 +32,62 @@ void KMP(const string& text, const string& vzorec) {
      * za izpis polja KMPnext lahko uporabite funkcijo izpis_KMPnext
      * Za izpis navidezne spremenljivke index uporabite: out << index << ' ';
      */
+    int* kmp_next = new int[vzorec.size()];
+    //cout << "size: " << vzorec.size() << endl;
+    for(int i = 0; i< vzorec.size(); i++){
+        kmp_next[i] = 0;
+    }
+
+    for(int i= 0; i < vzorec.size(); i++){
+        if(i == 0) kmp_next[i] = -1;
+        else if(i == 1) kmp_next[i] = 0;
+        else if (i >= 2){
+            int temp_size = i-1;
+            for(int j = 0; j < i; j++){
+
+                if(temp_size == 0){
+                    kmp_next[i] = 0;
+
+                    /*cout << "----------------" << endl;
+                    cout << "index: "<< i << endl;
+                    cout << "size: "<< temp_size << endl;
+                    cout << "s1: " << endl;
+                    cout << "s2: " << endl;
+                    cout << "----------------" << endl;
+                    */
+                    break;
+                }
+                string s1 = "";
+                string s2 = "";
+                for(int k = 0 ; k < temp_size; k++){
+                    s1 = s1 + vzorec[k];
+                }
+                int counter = 0;
+                for(int k = i-1; k > 0 && counter < temp_size;k--){
+                    s2 =vzorec[k] + s2 ;
+                    counter ++;
+                }
+
+                if(s1 == s2){
+                    kmp_next[i] = s1.size();
+
+                    /*cout << "----------------" << endl;
+                    cout << "index: "<< i << endl;
+                    cout << "size: "<< temp_size << endl;
+                    cout << "s1: " << s1 << endl;
+                    cout << "s2: " << s2 << endl;
+                    cout << "----------------" << endl;
+                    */
+                    break;
+                }
+                temp_size--;
+            }
+        }
+    }
+    
 }
 
 void Sunday(const string& text, const string& vzorec){
-
 }
 
 int main(int argc, const char *const argv[]) {
